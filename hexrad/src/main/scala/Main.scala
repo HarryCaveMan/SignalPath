3081988cd44d5f4f230ee1cb3f8f62834dc831ba
Harris <harris.joseph.088@gmail.com>
2021-04-12 21:03:17 -0400

    wrote boilerplate hex parser which is a million times better

--- hexrad/src/main/scala/Main.scala
+++ hexrad/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import java.util.Base64
+import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
   if( args.length != 1) {
@@ -20,8 +21,22 @@ object Main extends App {
   
   def encode_hex(input:String):String = {
     // Decode hex string into byte array (uses BigInteger because Int (8 bit signed?) is too small)
-    val as_bytes:Array[Byte] = new java.math.BigInteger(input,16)
+    // Should probably come up with a boilerplate parser that is not subject to overflow errors
+    //val as_bytes:Array[Byte] = new java.math.BigInteger(input,16).toByteArray()
+    
+    val as_bytes:Array[Byte] = hex_2_bytes(input)
     // Re-encode byte array into base64 representation
     Base64.getEncoder().encodeToString(as_bytes)
+  }
+  // This is much better than the old method
+  // because:
+  //  it only uses O(1) memory
+  //  It The overflow limit is the ssize limit of Array and ArrayBuffer
+  def hex_2_bytes(input:String):Array[Byte] = {
+    val bytes:ArrayBuffer[Byte] = ArrayBuffer[Byte]() 
+    for (byte_string <- input.grouped(2)) {
+      bytes += java.lang.Long.parseLong(byte_string,16).toByte
+    }
+    bytes.toArray
   }
 }
