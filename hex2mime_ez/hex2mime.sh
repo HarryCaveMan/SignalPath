96a6d1f47247a2bf630776dd6f7114cc3f019aa9
HarryCaveMan <harris.joseph.088@gmail.com>
2021-04-13 20:56:05 -0400

    comments with links to source implementations
    comments with links to source implementations

--- hex2mime_ez/hex2mime.sh
+++ hex2mime_ez/hex2mime.sh
@@ -2,7 +2,13 @@
 
 hex2mime() {
     # Thanks pals! https://superuser.com/a/416630
+    # https://linux.die.net/man/1/xxd
+    # vim hexdum implementation (xxd)
+    # https://github.com/vim/vim/blob/master/src/xxd/xxd.c
     # https://linux.die.net/man/1/base64
+    # gnu base64 implementation
+    # https://github.com/coreutils/gnulib/blob/master/lib/base64.c
+   
     base_64=`echo $1 | xxd -r -p | base64`
     echo $base_64
     return 0
